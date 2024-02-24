a2b6e6b322278e874a1a943eaeb8a5486ff87970
Saravana <saravana.m.architect@gmail.com>
2024-02-24 13:01:55 +0530

    Added NextJS base project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <b>Welcome to Sample NextJS</b>
+    </main>
+  );
+}
